caec43c80618cba26929da312f2cb0a2fd663704
andraaspar <andraaspar@gmail.com>
2014-06-20 16:49:24 +0200

    Now relies on grunt-sas for copying dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,11 @@
 module.exports = function(grunt) {
 	
-	var bowerComponents = grunt.file.expand('bower_components/*');
-	var bowerLibFiles = (function() {
-		var result = [];
-		for (var i = 0; i < bowerComponents.length; i++) {
-			result.push({expand: true, cwd: bowerComponents[i] + '/src', src: ['**/*'], dest: 'lib/'});
-		}
-		return result;
-	})();
-	
 	grunt.initConfig({
 		_outJS: 'build/test1/script/test.js',
 		_outCSS: 'build/test1/style/test.css',
 		
 		clean: {
-			update: ['lib/*'],
 			test1: ['<%= _outJS %>', '<%= _outCSS %>']
-		},
-		copy: {
-			update: {
-				files: bowerLibFiles
-			}
 		},
 		less: {
 			test1: {
@@ -36,10 +21,13 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		sas: {
+			update: {}
+		},
 		shell: {
 			update: {
 				command: [
-					'bower prune --force',
+					'bower prune',
 					'bower install',
 					'bower update'
 				].join('&&')
@@ -48,12 +36,12 @@ module.exports = function(grunt) {
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-typescript');
 	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-sas');
 
-	grunt.registerTask('update', ['shell:update','clean:update','copy:update']);
+	grunt.registerTask('update', ['shell:update','sas:update']);
 	grunt.registerTask('compile', ['clean:test1','typescript:test1','less:test1']);
 	grunt.registerTask('default', ['compile']);
 };
